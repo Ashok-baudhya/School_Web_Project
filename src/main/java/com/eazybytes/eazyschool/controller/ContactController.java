2fbdb9c78e196570f6e056860d82f1d56c4fda45
Ashok baudhya <abaudhya@gmail.com>
2024-02-15 14:51:36 +0530

    holiday page update and it's model and controller also added with update css

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.eazybytes.eazyschool.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,31 +10,51 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.eazybytes.eazyschool.model.Contact;
+import com.eazybytes.eazyschool.services.ContactService;
+
 
 @Controller
 public class ContactController {
 	
 	private static Logger log = LoggerFactory.getLogger(ContactController.class);
 
+	private final ContactService contactService;
 
+	@Autowired
+	public ContactController(ContactService contactService)
+	{
+		this.contactService = contactService;
+	}
+	
 	@RequestMapping("/contact")
 	public String contactView()
 	{
 		return "contact.html";
 	}
 
+//	@PostMapping(value ="/saveMsg")
+//	public ModelAndView saveMessage(@RequestParam String name ,@RequestParam String mobileNum, @RequestParam String email
+//									,@RequestParam String subject , @RequestParam String message)
+//	{
+//		log.info("Name : " + name);
+//		log.info("Mobile Number : " + mobileNum);
+//		log.info("Email Address : " + email);
+//		log.info("Subject : " + subject);
+//		log.info("Message : " + message);
+//		
+//		return new ModelAndView("redirect:/contact");
+//	}
+	
+	
 	@PostMapping(value ="/saveMsg")
-	public ModelAndView saveMessage(@RequestParam String name ,@RequestParam String mobileNum, @RequestParam String email
-									,@RequestParam String subject , @RequestParam String message)
+	public ModelAndView saveMessage(Contact contact)
 	{
-		log.info("Name : " + name);
-		log.info("Mobile Number : " + mobileNum);
-		log.info("Email Address : " + email);
-		log.info("Subject : " + subject);
-		log.info("Message : " + message);
-		
+		contactService.saveMessageDetails(contact);
 		return new ModelAndView("redirect:/contact");
 	}
+	
+	
 	
 	
 }
---
+++ src/main/java/com/eazybytes/eazyschool/services/ContactService.java
@@ -0,0 +1,28 @@
+package com.eazybytes.eazyschool.services;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.eazybytes.eazyschool.controller.ContactController;
+import com.eazybytes.eazyschool.model.Contact;
+
+@Service
+public class ContactService {
+	
+	
+	private static Logger log = LoggerFactory.getLogger(ContactService.class);
+
+
+	public boolean saveMessageDetails(Contact contact)
+	{
+		boolean isSaved = true;
+		
+		log.info(contact.toString());
+		return isSaved;
+		
+	}
+	
+}
